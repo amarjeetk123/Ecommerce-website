4cc799b42cb0eaf8d5571324cdbb48ee9f285f5a
amarjeetk123 <ak123del@gmail.com>
2022-12-21 12:44:36 +0530

    creating a controller for deleting the existing  collection

--- controller/collection.controller.js
+++ controller/collection.controller.js
@@ -55,7 +55,53 @@ export const updatecollection = asyncHandler(async (req,res) => {
             name:new_name,
         },
         {
-            new: true
+            new: true,
+            runValidators: true,
         }
     )
+
+    if(!updatecollection){
+        throw new CustomError( "Collection not found!...."  , 400)
+    }
+
+    // send responce to front end
+    res.status(200).json({
+        success: true,
+        message: "Collection updated successfully",
+        updatecollection,
+    })
+})
+
+
+
+/***********************************************************************************
+ * @Delete_Collection
+ * @routr :- http://localhost:4000/api/collection/delete
+ * @description :- collection_id
+ * @parameter :- 
+ * @returms :- 
+ ***********************************************************************************/
+export const deleteCollection = asyncHandler(async (req,res) => {
+    // existing value to be updates
+    const {id: collectionId} = req.params
+   
+
+    if(!new_name){
+        throw new CustomError( "new_name is required for updating the collection"  , 400)
+    }
+
+    let deleteCollection = await Collection.findByIdAndDelete( collectionId )
+
+    if(!deleteCollection){
+        throw new CustomError( "Collection not found!...."  , 400)
+    }
+
+    // freedup the memory
+    deleteCollection.remove()  // here we can also use .delete() // this is a option thing // it  will actually delete the variable
+    // send responce to front end
+    res.status(200).json({
+        success: true,
+        message: "Collection Deleted successfully",
+        
+    })
 })
