09ef3d2320ed079d73972365b25de4d3d76ce408
amarjeetk123 <ak123del@gmail.com>
2023-03-30 21:51:17 +0530

    doing some modifications

--- frontend/src/pages/RegisterPage.js
+++ frontend/src/pages/RegisterPage.js
@@ -1,16 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom"
 
 const RegisterPage = () => {
     return (
-        <div className="d-flex justify-content-center align-items-center" style={{height:"100vh"}}>
-            <div className="py-4 px-4 border border-primary">
+        <div className="main-register d-flex justify-content-center align-items-center" style={{ height: "100vh", backgroundImage: URL }}>
+            <div className="py-4 px-4 ">
                 <label className="heading text-center text-center w-100 fs-1 fw-bold">Register</label>
                 <h1 className="email fs-3 ">Full Name:</h1>
-                <input className="nameBox px-1" />
+                <input className="nameBox px-1 border  rounded-1 py-1" />
                 <h1 className="email fs-3 mt-2   ">Email:</h1>
-                <input className="inputBox px-1" />
+                <input className="inputBox px-1 border rounded-1 py-1" />
                 <h1 className="password fs-3 mt-2">Password:</h1>
-                <input className="passwordBox px-1" />
+                <input className="passwordBox px-1 border rounded-1 py-1" />
+
+                <Link to="/login" style={{ textDecoration: "none" }} >
+                    <button className="logi-btn">Login</button>
+                </Link>
+
             </div>
         </div>
 
